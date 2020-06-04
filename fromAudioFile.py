86b8691070d4136b28de8b74cb0e5f7267e5569e
ClownMonster <mk6386223@gmail.com>
2020-06-04 12:16:25 +0530

    from audio file script done

---
+++ fromAudioFile.py
@@ -0,0 +1,18 @@
+'''
+Gets the Text out of an English Audio File
+
+'''
+import speech_recognition as sr 
+
+r = sr.Recognizer()
+
+def convert_to_text(audioFile):
+    with sr.AudioFile(audioFile) as source:
+        audioData = r.record(source)
+        text = r.recognize_google(audioData)
+        print("\ntext : ", text)
+
+
+if __name__ == "__main__":
+    file_path = input("Enter the realtive path to audio file: ") 
+    convert_to_text(file_path)
