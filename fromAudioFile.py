07a4f7f7d18876bf90d03d00e66d0fb0fbc87518
ClownMonster <mk6386223@gmail.com>
2020-06-04 12:18:08 +0530

    comment of audio file

--- fromAudioFile.py
+++ fromAudioFile.py
@@ -15,4 +15,5 @@ def convert_to_text(audioFile):
 
 if __name__ == "__main__":
     file_path = input("Enter the realtive path to audio file: ") 
+    # 16-122828-0002.wav is the audio file u can use any
     convert_to_text(file_path)
