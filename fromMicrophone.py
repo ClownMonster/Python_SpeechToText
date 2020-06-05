376fc60f8aa6d31f3ad04cc6093c61413465e629
ClownMonster <mk6386223@gmail.com>
2020-06-05 10:34:00 +0530

    from Microphone script done

--- fromMicrophone.py
+++ fromMicrophone.py
@@ -3,3 +3,18 @@ This reads the data from the Microphone and converts into the textData and
 renders out the text 
 
 '''
+import speech_recognition as sr 
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Say Something: ")
+    audioData = r.listen(source)
+    print("\n[Recognizing]...")
+    try:
+        textData = r.recognize_google(audioDat)
+        print("You said : ",textData)
+    except:
+        print("Could Not recognize your voice")
+
+
